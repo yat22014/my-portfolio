e8ad4f8a72d107270576518ed208dcefb17bab2d
Frank <yat22011@gmail.com>
2023-06-17 22:13:57 +0800

    feat: add company logo to experince page and fix responsive background image

--- src/pages/experiences/index.tsx
+++ src/pages/experiences/index.tsx
@@ -1,58 +1,114 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { Chrono } from "react-chrono";
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { Chrono, TimelineItem } from "react-chrono";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Experiences() {
-  const expList = [
-    {
-      title: "Instelar Limited",
-      cardTitle: "Full stack developer - Instelar Limited",
-      cardSubtitle: "07/2022 - present",
-      cardDetailedText: ["paragraph1", "paragraph2"],
-      timelineContent: <div>Custom content</div>,
-    },
-    {
-      title: "COD Payment Limited",
-      cardTitle: "Software Engineer - COD Payment Limited",
-      cardSubtitle: "04/2021 - 07/2022",
-      cardDetailedText: ["paragraph1", "paragraph2"],
-      timelineContent: <div>Custom content</div>,
-    },
-    {
-      title: "Midas FMS Limited",
-      cardTitle: "Full stack developer - Midas FMS Limited",
-      cardSubtitle: "10/2020 - 03/2021",
-      cardDetailedText: ["paragraph1", "paragraph2"],
-      timelineContent: <div>Custom content</div>,
-    },
-    {
-      title: "Big Dipper Studio Limited",
-      cardTitle: "Analyst programmer - Big Dipper Studio Limited",
-      cardSubtitle: "08/2019 - 07/2020",
-      cardDetailedText: ["paragraph1", "paragraph2"],
-      timelineContent: <div>Custom content</div>,
-    },
-    {
-      title: "Eastern Color International Limited",
-      cardTitle: "Programmer - Eastern Color International Limited",
-      cardSubtitle: "02/2017 - 07/2019",
-      cardDetailedText: ["paragraph1", "paragraph2"],
-      timelineContent: <div>Custom content</div>,
-    }
-  ];
-  return (
-    <>
-      <main >
-      <div className='timeline-div'>
+    const expList: TimelineItem[] = [
+        {
+            title: "07/2022",
+            cardTitle: "Instelar Limited",
+            cardSubtitle: "Full stack developer",
+            cardDetailedText: ["paragraph1", "paragraph2"],
+            timelineContent: <div>Custom content</div>,
+            url: "https://www.instelar.com/en",
+            media: {
+              name: "instelar",
+              source: {
+                url: "images/company/instelar.jpg"
+              },
+              type: "IMAGE"
+            }
+          },
+          {
+            title: "04/2021",
+            cardTitle: "COD Payment Limited",
+            cardSubtitle: "Software Engineer",
+            cardDetailedText: ["paragraph1", "paragraph2"],
+            timelineContent: <div>Custom content</div>,
+            url: "https://www.codpayment.com/en",
+            media: {
+              name: "cod",
+              source: {
+                url: "images/company/cod.jpg"
+              },
+              type: "IMAGE"
+            }
+          },
+          {
+            title: "10/2020",
+            cardTitle: "Midas FMS Limited",
+            cardSubtitle: "Full stack developer",
+            cardDetailedText: ["paragraph1", "paragraph2"],
+            timelineContent: <div>Custom content</div>,
+            url: "https://midas-asia.com/",
+            media: {
+              name: "midas",
+              source: {
+                url: "images/company/midas.png"
+              },
+              type: "IMAGE"
+            }
+          },
+          {
+            title: "08/2019",
+            cardTitle: "Big Dipper Studio Limited",
+            cardSubtitle: "Analyst programmer",
+            cardDetailedText: ["paragraph1", "paragraph2"],
+            timelineContent: <div>Custom content</div>,
+            url: "https://www.bds.hk/",
+            media: {
+              name: "big dipper",
+              source: {
+                url: "images/company/bigDipper.jpg"
+              },
+              type: "IMAGE"
+            }
+          },
+          {
+            title: "02/2017",
+            cardTitle: "Eastern Color International Limited",
+            cardSubtitle: "Programmer",
+            cardDetailedText: ["paragraph1", "paragraph2"],
+            timelineContent: <div>Custom content</div>,
+            url: "https://www.eastern-color.com/home",
+            media: {
+              name: "eastern color",
+              source: {
+                url: "images/company/easternColor.png"
+              },
+              type: "IMAGE"
+            }
+        },
+    ];
 
-      <Chrono items={expList} mode="VERTICAL" />
-
-          </div>
-      </main>
-    </>
-  )
+    return (
+        <>
+            <main>
+                <div className="timeline-container">
+                    <div className="timeline-div">
+                        <Chrono
+                            items={expList}
+                            mediaSettings={{ imageFit: 'contain' }}
+                            mode="VERTICAL"
+                            theme={{
+                                // primary: "red",
+                                // secondary: "blue",
+                                cardBgColor: "yellow",
+                                titleColor: "white",
+                                titleColorActive: "blue",
+                            }}
+                            // classNames={{
+                            //   cardMedia: 'my-card-media',
+                            // }}
+                            scrollable 
+                        />
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 }
