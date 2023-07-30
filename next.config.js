339e0120bada526b42125e673be67049473e128f
Frank <yat22011@gmail.com>
2023-07-31 00:58:19 +0800

    ci: rollback next config

--- next.config.js
+++ next.config.js
@@ -3,28 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-// module.exports = nextConfig
-
-// next.config.js
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
-  },
-  reactStrictMode: true,
-}
+module.exports = nextConfig
