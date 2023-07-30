c843f719d55074c90e62b504fac211fc69fb77ef
Frank <yat22011@gmail.com>
2023-07-30 19:27:05 +0800

    ci: add deploy config file

--- next.config.js
+++ next.config.js
@@ -3,4 +3,28 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+// next.config.js
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
+  },
+  reactStrictMode: true,
+}
